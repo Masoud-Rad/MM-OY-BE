6ac581a8060e9c15ceb229719fac5800f325bcb5
Masoud <masoud.rad.uk@gmail.com>
2025-01-05 19:43:54 +0000

    logo url is updated

--- db/data/development-data/logo.js
+++ db/data/development-data/logo.js
@@ -1,3 +1,3 @@
 module.exports = [
-  {company_name : 'MM-onkeydown', logo_url: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.klipartz.com%2Fen%2Fsearch%3Fq%3Dlion%2BLogo&psig=AOvVaw3hrxp_xBcmp4qtJaUDKAic&ust=1730761304374000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJDrzb6iwYkDFQAAAAAdAAAAABBh' }
-];
+  {company_name : 'MM-onkeydown', logo_url: "https://i.pinimg.com/originals/67/69/bb/6769bbdfd658b026566bd5e6ca924ad7.png"}
+   ];
