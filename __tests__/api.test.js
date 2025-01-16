9e5acfa6f50e92226c51b7ae1fe2ab46c8376f9a
Masoud <masoud.rad.uk@gmail.com>
2025-01-16 19:58:31 +0000

    some update

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -43,7 +43,7 @@ describe("Getting mainPageData", () => {
     return request(app)
       .get("/api/mainPageData")
       .expect(200)
-      .then((response) => {
+      .then((response) => { console.log(response.body.mainPageData[0]);
         const result = response.body.mainPageData[0];
         expect(typeof result).toBe("object");
         expect(result.hasOwnProperty("title")).toBe(true);
